a99c70c4fca44cf97d86db2849cea5f9af7c90b3
Lucas Pimentel <lulalucas.27@gmail.com>
2025-12-22 21:47:03 -0300

    fix: fixes bug in the scanner that considered * a LEFT_PAREN

--- src/lite/Scanner.java
+++ src/lite/Scanner.java
@@ -63,7 +63,7 @@ public class Scanner {
             case '-': addToken(MINUS);break;
             case '+': addToken(PLUS);break;
             case ';': addToken(SEMICOLON);break;
-            case '*': addToken(LEFT_PAREN);break;        
+            case '*': addToken(STAR);break;        
             
             case '!': addToken(match('=')?BANG_EQUAL:BANG);break;
             case '=': addToken(match('=')?EQUAL_EQUAL:EQUAL);break;
