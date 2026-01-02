752c024fbb628de1ed35677a65783e31621420f9
Lucas Pimentel <lulalucas.27@gmail.com>
2026-01-02 19:17:01 -0300

    feat: name metaprogramming visitor boilerplate generator script generic to acommodate statement implementation

--- scripts/generate_ast_classes.py
+++ scripts/generate_ast_classes.py
@@ -2,35 +2,29 @@ import sys
 from pathlib import Path
 
 if len(sys.argv) != 2:
-    print("Usage: python3 generate_expr.py <dir_path>")
+    print("Usage: python3 generate.py <dir_path>")
     exit()
 
 dir_path = sys.argv[1]
 
-types = {
-    "Binary"   : ["Expr left", "Token operator", "Expr right"],
-    "Unary"    : ["Token operator", "Expr right"],
-    "Grouping" : ["Expr expression"],
-    "Literal"  : ["Object value"]
-}
 
-
-def define_ast(types):
-    file_name = dir_path+"/Expr.java"
+## Generates files with Visitor boilerplate
+def define_ast(BASE,types):
+    file_name = dir_path+"/"+BASE+".java"
     with open(file_name,"w") as f:
         print("package lite;",file=f)
         print("",file=f)
-        print("abstract class Expr{",file=f)
+        print("abstract class "+BASE+"{",file=f)
         print("",file=f)
         print("\tabstract <T> T accept(Visitor<T> visitor);",file=f)
         print("",file=f)
         print("\tinterface Visitor<T> {",file=f)
         for type,attributes in types.items():
-            print("\t\tT visit"+type+"Expr("+type+" expr);",file=f)
+            print("\t\tT visit"+type+BASE+"("+type+" "+BASE.lower()+");",file=f)
         print("\t}",file=f)
         print("",file=f)
         for type,attributes in types.items():
-            print("\tstatic class "+type+" extends Expr {",file=f)
+            print("\tstatic class "+type+" extends "+BASE+" {",file=f)
             constructor_args = ", ".join(attributes)
             print("\t\t"+type+"("+constructor_args+"){",file=f)
             for attribute in attributes:
@@ -41,15 +35,24 @@ def define_ast(types):
                 print("\t\tfinal "+attribute+";",file=f)
             print("\t\t@Override",file=f)
             print("\t\t<T> T accept(Visitor<T> visitor){",file=f)
-            print("\t\t\treturn visitor.visit"+type+"Expr(this);",file=f)
+            print("\t\t\treturn visitor.visit"+type+BASE+"(this);",file=f)
             print("\t\t}",file=f)
             print("\t}",file=f)
                 
         print("}",file=f)
-        
-        
 
 
 if __name__=="__main__":
-    define_ast(types)
+    define_ast("Stmt", {
+        "Expression":["Expr expression"],
+        "Print":["Expr expression"],
+    })
+    define_ast("Expr", {
+        "Binary"   : ["Expr left", "Token operator", "Expr right"],
+        "Unary"    : ["Token operator", "Expr right"],
+        "Grouping" : ["Expr expression"],
+        "Literal"  : ["Object value"]
+    })
+
+
 
